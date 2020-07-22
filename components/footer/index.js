82625d43bce01137bc77bc32492e273d066e9f2e
Tabizel <64487028+Tabizel@users.noreply.github.com>
2020-07-22 16:55:05 -0300

    Add link on footer

--- components/footer/index.js
+++ components/footer/index.js
@@ -9,7 +9,7 @@ const Footer = () => {
           <p className='font-bold text-sm'><a target='_blank' href='https://www.linkedin.com/in/gabriel-miranda-63729038/'>LinkedIn</a> /
           <a target='_blank' href='https://github.com/Tabizel'> GitHub</a>
           </p>
-          <img className='w-24 inline-block text-left' src='/images/logo_devpleno.png' alt='Dev Pleno' />
+          <a target='_blank' href='https://devpleno.com/'><img className='w-24 inline-block text-left' src='/images/logo_devpleno.png' alt='Dev Pleno' /></a>
         </div>
       </div>
     </div >
