b62866708be7f64b1acd5d92de8241b4b2a1c806
Tabizel <64487028+Tabizel@users.noreply.github.com>
2020-07-22 16:43:47 -0300

    Adding private key to base64

--- pages/api/msg-contato.js
+++ pages/api/msg-contato.js
@@ -6,12 +6,17 @@ const genProtocolo = () => {
   return code.substring(1, 5) + '-' + code.substring(6, 10) + '-' + code.substring(8, 12)
 }
 
+const fromBase64 = value => {
+  const buff = Buffer.from(value, 'base64')
+  return buff.toString('ascii')
+}
+
 export default async (req, res) => {
 
   try {
     await doc.useServiceAccountAuth({
       client_email: process.env.SHEET_CLIENT_EMAIL,
-      private_key: process.env.SHEET_PRIVATE_KEY
+      private_key: fromBase64(process.env.SHEET_PRIVATE_KEY)
     })
     await doc.loadInfo()
     const sheet = doc.sheetsByIndex[4]
